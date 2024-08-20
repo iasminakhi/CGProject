bd3716ff361c69eda8c9180125b113673cc17fa8
iasminakhi <bipashaiasmin8@gmail.com>
2024-08-20 12:22:23 +0600

    upload

--- Menu.cpp
+++ Menu.cpp
@@ -14,10 +14,10 @@ void Menu(){
      outtextxy(100,100,"1. Myname.");
      outtextxy(100,150,"2. Flag.");
      outtextxy(100,200,"3. Hut.");
-     outtextxy(100,250,"4. Mid Point Circle");
-     outtextxy(100,300,"5. Emoji.");
-     outtextxy(100,350,"6. Scenary.");
-     outtextxy(100,400,"7. Moving Car.");
+     outtextxy(100,250,"4. Emoji.");
+     outtextxy(100,300,"5. Scenary.");
+     outtextxy(100,350,"6. Moving Car.");
+     outtextxy(100,400,"7. Mid Point Circle");
      outtextxy(100,450,"8. Exit.");
 
 }
@@ -91,50 +91,6 @@ void hut()
 
         getch();
      
-}
-
-void midpointcircle()
-{
-    cleardevice();
-
-initwindow(800,800,"Midpointcircle");
-outtextxy(300,100,"Bipasha Iasmin");
-
-int x,y,r;
-cin>>x>>y>>r;
-
-int x1 = 0;
-int y1 = r;
-int p0=1-r;
-putpixel(x,y,7);
-while(x1<y1)
-{
-
-x1++;
-if(p0<0){
-
-p0=p0+(2*x1)+1;
-}
-else{
-y1--;
-p0=p0+(2*x1)+1-(2*y1);
-}
-cout<<x1<<"..."<<y1<<endl;
-
-putpixel(x+x1,y+y1,7);
-putpixel(x+x1,y-y1,7);
-putpixel(x-x1,y+y1,7);
-putpixel(x-x1,y-y1,7);
-putpixel(y+y1,x+x1,7);
-putpixel(y+y1,x-x1,7);
-putpixel(y-y1,x+x1,7);
-putpixel(y-y1,x-x1,7);
-delay(100);
-
-}
-getch();
-closegraph();
-
 }
 
 void emoji()
@@ -406,6 +362,50 @@ void moving_car()
     
 }
 
+void midpointcircle()
+{
+    cleardevice();
+
+initwindow(800,800,"Midpointcircle");
+outtextxy(300,100,"Bipasha Iasmin");
+
+int x,y,r;
+cin>>x>>y>>r;
+
+int x1 = 0;
+int y1 = r;
+int p0=1-r;
+putpixel(x,y,7);
+while(x1<y1)
+{
+
+x1++;
+if(p0<0){
+
+p0=p0+(2*x1)+1;
+}
+else{
+y1--;
+p0=p0+(2*x1)+1-(2*y1);
+}
+cout<<x1<<"..."<<y1<<endl;
+
+putpixel(x+x1,y+y1,7);
+putpixel(x+x1,y-y1,7);
+putpixel(x-x1,y+y1,7);
+putpixel(x-x1,y-y1,7);
+putpixel(y+y1,x+x1,7);
+putpixel(y+y1,x-x1,7);
+putpixel(y-y1,x+x1,7);
+putpixel(y-y1,x-x1,7);
+delay(100);
+
+}
+getch();
+closegraph();
+
+}
+
 int main()
 {
     int gd = DETECT, gm;
@@ -445,15 +445,7 @@ int main()
             delay(1000);
             cleardevice();
             break;
-        case '4':
-            outtextxy(200,400,"You chose option 4!");
-            delay(1000);
-            cleardevice();
-            midpointcircle();
-            delay(1000);
-            cleardevice();
-            break;
-        case '5':
+       case '4':
             outtextxy(200,400,"You chose option 4!");
             delay(1000);
             cleardevice();
@@ -461,7 +453,7 @@ int main()
             delay(1000);
             cleardevice();
             break;
-        case '6':
+        case '5':
             outtextxy(200,400,"You chose option 5!");
             delay(1000);
             cleardevice();
@@ -469,11 +461,19 @@ int main()
             delay(1000);
             cleardevice();
             break;
-        case '7':
-            outtextxy(200,400,"You chose option 5!");
+        case '6':
+            outtextxy(200,400,"You chose option 6!");
             delay(1000);
             cleardevice();
             moving_car();
+            delay(1000);
+            cleardevice();
+            break;
+        case '7':
+            outtextxy(200,400,"You chose option 7!");
+            delay(1000);
+            cleardevice();
+            midpointcircle();
             delay(1000);
             cleardevice();
             break;
